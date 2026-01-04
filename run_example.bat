43d655ec75ab9db80ed4474d4493a8fafcf18cce
ooVientoo <64309002+Vientofficial@users.noreply.github.com>
2026-01-04 13:24:19 +0800

    Update run_example.bat

--- run_example.bat
+++ run_example.bat
@@ -1,2 +1,12 @@
 @echo off
-echo example.pl0 | pl0.exe
+
+@REM echo example.pl0 | pl0.exe
+
+
+@REM echo while.pl0 | pl0.exe
+
+@REM echo write.pl0 | pl0.exe
+
+@REM echo const.pl0 | pl0.exe
+
+echo note.pl0 | pl0.exe
